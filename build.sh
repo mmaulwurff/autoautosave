6fed7cd068d02d69db27a819b844a3acf9979259
Alexander <mmaulwurff@gmail.com>
2018-06-16 18:01:03 +0700

    add voice, add cvars

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
-acc source/autoautosave.acs acs/autoautosave.o && \
-    rm -f autoautosave.pk3 && \
-    zip autoautosave.pk3 \
-        acs/autoautosave.o \
-        loadacs.txt \
-        README.txt \
-        source/autoautosave.acs \
-        mapinfo.txt zscript.txt \
-        keyconf.txt menudef.txt \
-        cvarinfo.txt && \
-    gzdoom -glversion 3 \
-           -file \
-           autoautosave.pk3 \
-           ~/Programs/Games/wads/maps/DOOMTEST.wad \
-           "$1" "$2" \
-           +map test \
-           -nomonsters
+acc source/autoautosave.acs acs/autoautosave.o \
+&& \
+rm -f autoautosave.pk3 \
+&& \
+zip autoautosave.pk3 \
+    acs/autoautosave.o \
+    *.txt \
+    *.md \
+    source/autoautosave.acs \
+    sounds/*.wav \
+&& \
+gzdoom -glversion 3 \
+       -file \
+       autoautosave.pk3 \
+       ~/Programs/Games/wads/maps/DOOMTEST.wad \
+       "$1" "$2" \
+       +map test \
+       -nomonsters
