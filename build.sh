906a9a9d2d186ce22d20e1d17f709ba7939c8e51
Alexander <mmaulwurff@gmail.com>
2018-07-01 18:31:14 +0700

    make changelog pretty

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 name=autoautosave
-
-git log --pretty=format:"-%d %ai %s%n" > changelog.txt \
+git log --date=short --pretty=format:"-%d %ad %s%n" | \
+    grep -v "^$" | \
+    sed "s/HEAD -> master, //" | \
+    sed "s/, origin\/master//" | \
+    sed "s/ (HEAD -> master)//" | \
+    sed "s/ (origin\/master)//"  |\
+    sed "s/- (tag: \(v\?[0-9.]*\))/\n\1\n-/" \
+    > changelog.txt \
 && \
 rm -f $name.pk3 \
 && \
