2e0224e8663b5e9396f37610767d26c66abd0390
Alexander Kromm <mmaulwurff@gmail.com>
2020-06-19 23:12:32 +0700

    rename sound files

--- scripts/gen-files.sh
+++ scripts/gen-files.sh
@@ -74,7 +74,7 @@ echo "// This file is generated automatically!"                       >  $sndinf
 echo "// All changed will be lost!"                                   >> $sndinfo_file
 echo "// see gen-files.sh for details."                               >> $sndinfo_file
 echo ""                                                               >> $sndinfo_file
-echo "$filtered" | awk '{ printf("aas/voice%s \"sounds/m8faas%s.ogg\"\n", $2, $2) }' >> $sndinfo_file
+echo "$filtered" | awk '{ printf("aas/voice%s \"sounds/aas%s.ogg\"\n", $2, $2) }' >> $sndinfo_file
 
 # Generate ogg sounds
 
@@ -83,7 +83,7 @@ rm -f sounds/*
 mkdir -p sounds
 
 params="-s140 -p0 -g1 -v english"
-filename="sounds/m8faas"
+filename="sounds/aas"
 
 while read -r line; do
     voice_string=$(echo $line | awk '{ print $10; for (i=11; i<NF; ++i) { printf(" %s", $i); } }')
