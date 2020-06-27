b943399440a7418b098ae361b0615a758f239cf6
Alexander Kromm <mmaulwurff@gmail.com>
2020-06-27 13:14:55 +0700

    clean up build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,11 @@ rm -f "$name"
 
 git log --date=short --pretty=format:"-%d %ad %s%n" | \
     grep -v "^$" | \
-    sed "s/HEAD -> master, //" | \
-    sed "s/, origin\/master//" | \
-    sed "s/ (HEAD -> master)//" | \
-    sed "s/ (origin\/master)//"  |\
-    sed "s/- (tag: \(v\?[0-9.]*\))/\n\1\n-/" \
+    sed "s/HEAD -> master, //"   | \
+    sed "s/, origin\\/master//"  | \
+    sed "s/ (HEAD -> master)//"  | \
+    sed "s/ (origin\\/master)//" | \
+    sed "s/- (tag: \\(v\\?[0-9.]*\\))/\\n\\1\\n-/" \
     > changelog.txt
 
 ./scripts/gen-files.sh
--- scripts/gen-files.sh
+++ scripts/gen-files.sh
@@ -1,82 +1,96 @@
 #!/bin/bash
 
+# This script generates ZScript class to get event information (class_file),
+# CVar definitions (cvar_file), sound definitions (sndinfo_file), and sounds
+# (sounds/*.ogg).
+#
+# Dependencies:
+# - espeak
+# - oggenc
+#
+# How to use:
+# ./scripts/gen-files.sh
+
 class_file=zscript/aas_event.zs
 cvar_file=cvarinfo.toggles.txt
 sndinfo_file=sndinfo.txt
 
-filtered=$(cat event_types.org | grep -v "|--" | grep "|" | grep -v "N |")
+filtered=$(grep -v "|--" event_types.org | grep "|" | grep -v "N |")
 
-# Generate ZScript code
-
-echo "// This file is generated from event_types.org by gen-files.sh."                                                                        >  $class_file
-echo "// All manual changes will be lost."                                                                                                    >> $class_file
-echo ""                                                                                                                                       >> $class_file
-echo "class aas_event"                                                                                                                        >> $class_file
-echo "{"                                                                                                                                      >> $class_file
-echo ""                                                                                                                                       >> $class_file
-echo "  enum event_type"                                                                                                                      >> $class_file
-echo "  {"                                                                                                                                    >> $class_file
-echo "$filtered" | awk '{ printf("    %-16s = %3s,\n", $4, $2) }'                                                                             >> $class_file
-echo "  }"                                                                                                                                    >> $class_file
-echo ""                                                                                                                                       >> $class_file
-echo "  static string message(int type)"                                                                                                      >> $class_file
-echo "  {"                                                                                                                                    >> $class_file
-echo "    switch (type)"                                                                                                                      >> $class_file
-echo "    {"                                                                                                                                  >> $class_file
-echo "$filtered" | awk '{ printf("      case %-16s : return \"%s", $4, $10); for (i=11; i<NF; ++i) { printf(" %s", $i); }; printf("\";\n") }' >> $class_file
-echo "      default: return \"Unknown event type.\";"                                                                                         >> $class_file
-echo "    }"                                                                                                                                  >> $class_file
-echo "  }"                                                                                                                                    >> $class_file
-echo ""                                                                                                                                       >> $class_file
-echo "  static string toggle_name(int type)"                                                                                                  >> $class_file
-echo "  {"                                                                                                                                    >> $class_file
-echo "    switch (type)"                                                                                                                      >> $class_file
-echo "    {"                                                                                                                                  >> $class_file
-echo "$filtered" | awk '{ printf("      case %-16s : return \"%s\";\n", $4, $6) }'                                                            >> $class_file
-echo "      default: return \"m8f_aas_false\";"                                                                                               >> $class_file
-echo "    }"                                                                                                                                  >> $class_file
-echo "  }"                                                                                                                                    >> $class_file
-echo ""                                                                                                                                       >> $class_file
-echo "  static string shot_toggle_name(int type)"                                                                                             >> $class_file
-echo "  {"                                                                                                                                    >> $class_file
-echo "    switch (type)"                                                                                                                      >> $class_file
-echo "    {"                                                                                                                                  >> $class_file
-echo "$filtered" | awk '{ printf("      case %-16s : return \"m8f_aas_shot_on_%s\";\n", $4, $4) }'                                            >> $class_file
-echo "      default: return \"m8f_aas_false\";"                                                                                               >> $class_file
-echo "    }"                                                                                                                                  >> $class_file
-echo "  }"                                                                                                                                    >> $class_file
-echo ""                                                                                                                                       >> $class_file
-echo "  static string voice_toggle_name(int type)"                                                                                             >> $class_file
-echo "  {"                                                                                                                                    >> $class_file
-echo "    switch (type)"                                                                                                                      >> $class_file
-echo "    {"                                                                                                                                  >> $class_file
-echo "$filtered" | awk '{ printf("      case %-16s : return \"m8f_aas_voice_on_%s\";\n", $4, $4) }'                                            >> $class_file
-echo "      default: return \"m8f_aas_true\";"                                                                                               >> $class_file
-echo "    }"                                                                                                                                  >> $class_file
-echo "  }"                                                                                                                                    >> $class_file
-echo ""                                                                                                                                       >> $class_file
-echo "} // class aas_event"                                                                                                                   >> $class_file
+# Generate ZScript code ############################################################################
+{
+    echo "// This file is generated from event_types.org by gen-files.sh."
+    echo "// All manual changes will be lost."
+    echo
+    echo "class aas_event"
+    echo "{"
+    echo
+    echo "  enum event_type"
+    echo "  {"
+    echo "$filtered" | awk '{ printf("    %-16s = %3s,\n", $4, $2) }'
+    echo "  }"
+    echo
+    echo "  static string message(int type)"
+    echo "  {"
+    echo "    switch (type)"
+    echo "    {"
+    echo "$filtered" | awk '{ printf("      case %-16s : return \"%s", $4, $10); for (i=11; i<NF; ++i) { printf(" %s", $i); }; printf("\";\n") }'
+    echo "      default: return \"Unknown event type.\";"
+    echo "    }"
+    echo "  }"
+    echo
+    echo "  static string toggle_name(int type)"
+    echo "  {"
+    echo "    switch (type)"
+    echo "    {"
+    echo "$filtered" | awk '{ printf("      case %-16s : return \"%s\";\n", $4, $6) }'
+    echo "      default: return \"m8f_aas_false\";"
+    echo "    }"
+    echo "  }"
+    echo
+    echo "  static string shot_toggle_name(int type)"
+    echo "  {"
+    echo "    switch (type)"
+    echo "    {"
+    echo "$filtered" | awk '{ printf("      case %-16s : return \"m8f_aas_shot_on_%s\";\n", $4, $4) }'
+    echo "      default: return \"m8f_aas_false\";"
+    echo "    }"
+    echo "  }"
+    echo
+    echo "  static string voice_toggle_name(int type)"
+    echo "  {"
+    echo "    switch (type)"
+    echo "    {"
+    echo "$filtered" | awk '{ printf("      case %-16s : return \"m8f_aas_voice_on_%s\";\n", $4, $4) }'
+    echo "      default: return \"m8f_aas_true\";"
+    echo "    }"
+    echo "  }"
+    echo
+    echo "} // class aas_event"
+} > $class_file
 
 # Generate cvarinfo
+{
+    echo "// This file is generated from event_types.org by gen-files.sh."
+    echo "// All manual changes will be lost."
+    echo
+    echo "$filtered" | awk '{ printf("server bool %-32s = %s;\n", $6, $8) }' | sort | uniq
+    echo
+    echo "$filtered" | awk '{ printf("server bool m8f_aas_shot_on_%-16s = false;\n", $4) }' | sort | uniq
+    echo
+    echo "$filtered" | awk '{ printf("server bool m8f_aas_voice_on_%-16s = true;\n", $4) }' | sort | uniq
+} > $cvar_file
 
-echo "// This file is generated from event_types.org by gen-files.sh."                                                                        >  $cvar_file
-echo "// All manual changes will be lost."                                                                                                    >> $cvar_file
-echo ""                                                                                                                                       >> $cvar_file
-echo "$filtered" | awk '{ printf("server bool %-32s = %s;\n", $6, $8) }' | sort | uniq                                                        >> $cvar_file
-echo ""                                                                                                                                       >> $cvar_file
-echo "$filtered" | awk '{ printf("server bool m8f_aas_shot_on_%-16s = false;\n", $4) }' | sort | uniq                                         >> $cvar_file
-echo ""                                                                                                                                       >> $cvar_file
-echo "$filtered" | awk '{ printf("server bool m8f_aas_voice_on_%-16s = true;\n", $4) }' | sort | uniq                                         >> $cvar_file
+# Generate sndinfo #################################################################################
+{
+    echo "// This file is generated automatically!"
+    echo "// All changed will be lost!"
+    echo "// see gen-files.sh for details."
+    echo
+    echo "$filtered" | awk '{ printf("aas/voice%s \"sounds/aas%s.ogg\"\n", $2, $2) }'
+} > $sndinfo_file
 
-# Generate sndinfo
-
-echo "// This file is generated automatically!"                       >  $sndinfo_file
-echo "// All changed will be lost!"                                   >> $sndinfo_file
-echo "// see gen-files.sh for details."                               >> $sndinfo_file
-echo ""                                                               >> $sndinfo_file
-echo "$filtered" | awk '{ printf("aas/voice%s \"sounds/aas%s.ogg\"\n", $2, $2) }' >> $sndinfo_file
-
-# Generate ogg sounds
+# Generate ogg sounds ##############################################################################
 
 rm -f sounds/*
 
@@ -86,14 +100,14 @@ params="-s140 -p0 -g1 -v english"
 filename="sounds/aas"
 
 while read -r line; do
-    voice_string=$(echo $line | awk '{ print $10; for (i=11; i<NF; ++i) { printf(" %s", $i); } }')
-    i=$(echo $line | awk '{ print $2 }')
+    voice_string=$(echo "$line" | awk '{ print $10; for (i=11; i<NF; ++i) { printf(" %s", $i); } }')
+    i=$(echo "$line" | awk '{ print $2 }')
     #echo $voice_string
-    espeak "$voice_string" $params -w $filename$i".wav";
+    espeak "$voice_string" $params -w "$filename$i.wav";
 done <<< "$filtered"
 
 for f in sounds/*.wav;
 do
-    oggenc -s 0 -Q -o ${f%.*}.ogg $f
-    rm $f
+    oggenc -s 0 -Q -o "${f%.*}.ogg" "$f"
+    rm "$f"
 done
